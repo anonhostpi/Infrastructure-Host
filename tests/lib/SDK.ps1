1fce194fe7acbf9caf515c1009fd595555fc0b13
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-01-19 22:46:14 -0700

    feat: introduce testing SDK

---
+++ tests/lib/SDK.ps1
@@ -0,0 +1,74 @@
+param(
+    [switch] $Globalize
+)
+
+New-Module -Name SDK -ScriptBlock {
+    param(
+        [bool] $Globalize = $false
+    )
+
+    . "$PSScriptRoot/helpers/PowerShell.ps1"
+
+    $SDK = New-Object PSObject -Property @{
+        Path = "$PSScriptRoot\SDK.ps1"
+    }
+
+    if ($Globalize) {
+        Set-Variable -Name SDK -Value $SDK -Scope Global
+    }
+
+    Add-ScriptMethods $SDK @{
+        Extend = {
+            param(
+                [Parameter(Mandatory = $true)]
+                [string]$ModuleName,
+                [Parameter(Mandatory = $true)]
+                $ModuleObject
+            )
+
+            $this | Add-Member -MemberType NoteProperty -Name $ModuleName -Value $ModuleObject -Force
+        }
+        Job = {
+            param(
+                [Parameter(Mandatory = $true)]
+                [scriptblock]$ScriptBlock,
+                [int] $Timeout,
+                [System.Collections.IDictionary] $Env = @{}
+            )
+            $job = Start-Job -ScriptBlock {
+                param(
+                    [string] $InnerJob,
+                    [System.Collections.IDictionary] $EnvVars,
+                    [string] $SDKPath
+                )
+
+                foreach( $name in $EnvVars.Keys ){
+                    Set-Variable -Name $name -Value $EnvVars[$name] -Scope Global
+                }
+
+                . $SDKPath
+
+                $sb = [scriptblock]::Create($InnerJob)
+                & $sb
+            } -ArgumentList $ScriptBlock.ToString(), $Env, $this.Path
+
+            # Return true if timed out, false otherwise
+            if( $Timeout -gt 0 ){
+                $completed = $job | Wait-Job -Timeout $Timeout
+                if( -not $completed ){
+                    Stop-Job $job | Out-Null
+                    return $true
+                }
+                return $false
+            } Else {
+                $job | Wait-Job | Out-Null
+                return $false
+            }
+        }
+    }
+
+    & "$PSScriptRoot/Network.ps1" -SDK $SDK
+    & "$PSScriptRoot/Vbox.ps1" -SDK $SDK
+
+    Export-ModuleMember -Variable SDK
+} -ArgumentList ([bool] $Globalize) | Import-Module -Force
