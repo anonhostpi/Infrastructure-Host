a2cc04896921912a606b56554ddfd539232ba828
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-01-20 19:47:33 -0700

    feat: add multipass worker declaration to SDK

--- tests/lib/Multipass.ps1
+++ tests/lib/Multipass.ps1
@@ -13,9 +13,190 @@ New-Module -Name SDK.Multipass -ScriptBlock {
 
     . "$PSScriptRoot\helpers\PowerShell.ps1"
 
+    $mod.Configurator = @{
+        Defaulter = {
+            param($Table, $Defaults)
+            $result = [ordered]@{}
+            $keys = (& {
+                $Defaults.Keys | ForEach-Object { $_ }
+                $Table.Keys | ForEach-Object { $_ }
+            }) | Sort-Object -Unique
+            foreach( $key in $keys ) {
+                $result[$key] = & $mod.Defaulter $Table.$key $Defaults.$key
+            }
+            return $result
+        }
+        Defaults = @{
+            CPUs = 2
+            Memory = "4G"
+            Disk = "40G"
+        }
+    }
+    $mod.Worker = @{
+        Properties = @{
+            #region: Worker COnfiguration
+            Rendered = {
+                $config = $this.Config
+                $defaults = $this.Defaults
+                if( $null -eq $defaults ){
+                    $defaults = $mod.Configurator.Defaults
+                }
+                $rendered = & $mod.Configurator.Defaulter $config $defaults
+                $this | Add-Member -MemberType NoteProperty -Name Rendered -Value $rendered -Force
+                return $rendered
+            }
+            Name = {
+                return $this.Rendered.Name
+            }
+            CPUs = {
+                return $this.Rendered.CPUs
+            }
+            Memory = {
+                return $this.Rendered.Memory
+            }
+            Disk = {
+                return $this.Rendered.Disk
+            }
+        }
+        Methods = @{
+            #region: Worker VM Info
+            Info = {
+                return $mod.SDK.Multipass.Info($this.Name)
+            }
+            Addresses = {
+                return $mod.SDK.Multipass.Addresses($this.Name)
+            }
+            Address = {
+                return $mod.SDK.Multipass.Address($this.Name)
+            }
+
+            #region: Worker VM Lifecycle
+            Exists = {
+                return $mod.SDK.Multipass.Exists($this.Name)
+            }
+            Running = {
+                return $mod.SDK.Multipass.Running($this.Name)
+            }
+            Create = {
+                $config = $this.Rendered
+                return $mod.SDK.Multipass.Create(
+                    $this.Name,
+                    $null,
+                    $null,
+                    $config.CPUs,
+                    $config.Memory,
+                    $config.Disk
+                )
+            }
+            Ensure = {
+                if( -not $this.Exists() ) {
+                    return $this.Create()
+                }
+                return $true
+            }
+            Destroy = {
+                if( -not $this.Exists() ) { return $true }
+
+                $name = $this.Name
+                $shutdown_called_successfully = $this.Shutdown($true)
+                if( -not $shutdown_called_successfully ) {
+                    throw "Failed to shutdown VM '$name'"
+                }
+                $timed_out = $this.UntilShutdown(60)
+                if( $timed_out ) {
+                    throw "Timed out waiting for VM '$name' to shutdown"
+                }
+                return $mod.SDK.Multipass.Destroy($name)
+            }
+            Start = {
+                return $mod.SDK.Multipass.Start($this.Name)
+            }
+            Shutdown = {
+                param( [bool]$Force )
+                return $mod.SDK.Multipass.Shutdown($this.Name, $Force)
+            }
+            UntilShutdown = {
+                param( [int] $TimeoutSeconds )
+                return $mod.SDK.Multipass.UntilShutdown($this.Name, $TimeoutSeconds)
+            }
+            
+            #region: Worker Cloud-Init
+            Status = {
+                return $mod.SDK.Multipass.Status($this.Name)
+            }
+            UntilInstalled = {
+                param(
+                    [int]$TimeoutSeconds = 600
+                )
+                return $mod.SDK.Multipass.UntilInstalled($this.Name, $TimeoutSeconds)
+            }
+            Setup = {
+                param( [bool]$FailOnTimeout )
+                $created = $this.Ensure()
+                if( -not $created ) {
+                    throw "Failed to create VM '$($this.Name)'"
+                }
+                $timedout = $this.UntilInstalled( $mod.SDK.Settings.Virtualization.TimeoutSeconds )
+                if( $timedout -and $FailOnTimeout ) {
+                    throw "Timed out waiting for VM '$($this.Name)' to finish installation"
+                }
+                return $timedout
+            }
+
+            #region: Worker File Sharing
+            Mount = {
+                param(
+                    [Parameter(Mandatory = $true)]
+                    [string]$SourcePath,
+                    [Parameter(Mandatory = $true)]
+                    [string]$TargetPath
+                )
+                return $mod.SDK.Multipass.Mount($this.Name, $SourcePath, $TargetPath)
+            }
+            Unmount = {
+                param(
+                    [string]$TargetPath
+                )
+                return $mod.SDK.Multipass.Unmount($this.Name, $TargetPath)
+            }
+            Mounts = {
+                return $mod.SDK.Multipass.Mounts($this.Name)
+            }
+            Mounted = {
+                param(
+                    [Parameter(Mandatory = $true)]
+                    [string]$HostPath
+                )
+                return $mod.SDK.Multipass.Mounted($this.Name, $HostPath)
+            }
+            Transfer = {
+                param(
+                    [Parameter(Mandatory = $true)]
+                    [string]$Source,
+                    [Parameter(Mandatory = $true)]
+                    [string]$Destination
+                )
+                return $mod.SDK.Multipass.Transfer($Source, $Destination)
+            }
+
+            #region: Worker Command Execution
+            Exec = {
+                param(
+                    [Parameter(Mandatory = $true)]
+                    [string]$Command,
+                    [string]$WorkingDir
+                )
+                return $mod.SDK.Multipass.Exec($this.Name, $Command, $WorkingDir)
+            }
+            Shell = {
+                return $mod.SDK.Multipass.Shell($this.Name)
+            }
+        }
+    }
+
     $Multipass = New-Object PSObject
 
-    #region: Main utility method
+    #region: Main utility methods
     Add-ScriptMethods $Multipass @{
         Invoke = {
 
@@ -24,6 +205,24 @@ New-Module -Name SDK.Multipass -ScriptBlock {
                 Output = $output
                 ExitCode = $LASTEXITCODE
             }
+        }
+        Worker = {
+            param(
+                [Parameter(Mandatory = $true)]
+                [ValidateScript({ $null -ne $_.Config })]
+                $Base
+            )
+
+            $worker = If( $Base -is [System.Collections.IDictionary] ){
+                New-Object PSObject -Property $Base
+            } Else {
+                $Base
+            }
+
+            Add-ScriptProperties $worker $mod.Worker.Properties
+            Add-ScriptMethods $worker $mod.Worker.Methods
+            
+            return $worker
         }
     }
 
