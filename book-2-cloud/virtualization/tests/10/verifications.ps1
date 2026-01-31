bff4dc52aa2af8bf4b206f95e8c1c588bec87bb2
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-01-31 16:31:43 -0700

    test(virtualization): add 9 test placeholders for layer 10

--- book-2-cloud/virtualization/tests/10/verifications.ps1
+++ book-2-cloud/virtualization/tests/10/verifications.ps1
@@ -5,5 +5,15 @@ New-Module -Name "Verify.Virtualization" -ScriptBlock {
     $mod = @{ SDK = $SDK }
     . "$PSScriptRoot\..\..\..\..\book-0-builder\host-sdk\helpers\PowerShell.ps1"
 
-    $SDK.Testing.Verifications.Register("virtualization", 10, [ordered]@{})
+    $SDK.Testing.Verifications.Register("virtualization", 10, [ordered]@{
+        "libvirt installed" = { param($Worker) }
+        "libvirtd service active" = { param($Worker) }
+        "QEMU installed" = { param($Worker) }
+        "libvirt default network" = { param($Worker) }
+        "multipass installed" = { param($Worker) }
+        "multipassd service active" = { param($Worker) }
+        "KVM available for nesting" = { param($Worker) }
+        "Launch nested VM" = { param($Worker) }
+        "Exec in nested VM" = { param($Worker) }
+    })
 } -ArgumentList $SDK
