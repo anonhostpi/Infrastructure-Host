c4503c465c90fec1747dae3608c5ecd2bbb79737
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-01-31 16:14:08 -0700

    feat(users): implement user exists and shell verification tests

--- book-2-cloud/users/tests/3/verifications.ps1
+++ book-2-cloud/users/tests/3/verifications.ps1
@@ -8,8 +8,22 @@ New-Module -Name "Verify.Users" -ScriptBlock {
     $username = $SDK.Settings.Identity.username
 
     $SDK.Testing.Verifications.Register("users", 3, [ordered]@{
-        "user exists" = { param($Worker) }
-        "user shell is bash" = { param($Worker) }
+        "user exists" = {
+            param($Worker)
+            $result = $Worker.Exec("id $username")
+            $SDK.Testing.Record(@{
+                Test = "6.3.1"; Name = "$username user exists"
+                Pass = ($result.Success -and $result.Output -match "uid="); Output = $result.Output
+            })
+        }
+        "user shell is bash" = {
+            param($Worker)
+            $result = $Worker.Exec("getent passwd $username | cut -d: -f7")
+            $SDK.Testing.Record(@{
+                Test = "6.3.1"; Name = "$username shell is bash"
+                Pass = ($result.Output -match "/bin/bash"); Output = $result.Output
+            })
+        }
         "user in sudo group" = { param($Worker) }
         "Sudoers file exists" = { param($Worker) }
         "Root account locked" = { param($Worker) }
