fc9e70f3c2a934d3757ca6073d0d845fb117061d
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-01-31 16:35:12 -0700

    test(cockpit): add 7 test placeholders for layer 11

--- book-2-cloud/cockpit/tests/11/verifications.ps1
+++ book-2-cloud/cockpit/tests/11/verifications.ps1
@@ -5,5 +5,13 @@ New-Module -Name "Verify.Cockpit" -ScriptBlock {
     $mod = @{ SDK = $SDK }
     . "$PSScriptRoot\..\..\..\..\book-0-builder\host-sdk\helpers\PowerShell.ps1"
 
-    $SDK.Testing.Verifications.Register("cockpit", 11, [ordered]@{})
+    $SDK.Testing.Verifications.Register("cockpit", 11, [ordered]@{
+        "Cockpit installed" = { param($Worker) }
+        "Cockpit socket enabled" = { param($Worker) }
+        "cockpit-machines installed" = { param($Worker) }
+        "Cockpit listening on port" = { param($Worker) }
+        "Cockpit web UI responds" = { param($Worker) }
+        "Cockpit login page" = { param($Worker) }
+        "Cockpit restricted to localhost" = { param($Worker) }
+    })
 } -ArgumentList $SDK
