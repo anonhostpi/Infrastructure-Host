522f8f5499db048887fecea1bd128dd346642268
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-01-31 16:13:49 -0700

    feat(users): add verification test shape for layer 3

--- book-2-cloud/users/tests/3/verifications.ps1
+++ book-2-cloud/users/tests/3/verifications.ps1
@@ -5,5 +5,13 @@ New-Module -Name "Verify.Users" -ScriptBlock {
     $mod = @{ SDK = $SDK }
     . "$PSScriptRoot\..\..\..\..\book-0-builder\host-sdk\helpers\PowerShell.ps1"
 
-    $SDK.Testing.Verifications.Register("users", 3, [ordered]@{})
+    $username = $SDK.Settings.Identity.username
+
+    $SDK.Testing.Verifications.Register("users", 3, [ordered]@{
+        "user exists" = { param($Worker) }
+        "user shell is bash" = { param($Worker) }
+        "user in sudo group" = { param($Worker) }
+        "Sudoers file exists" = { param($Worker) }
+        "Root account locked" = { param($Worker) }
+    })
 } -ArgumentList $SDK
