a7dc8854f32adaec0aec25bf4b83e53fb276c9a5
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-02-01 11:20:21 -0700

    refactor: move Invoke-AutoinstallTest.ps1 to book-1-foundation
    Update SDK path to reference book-0-builder from new location.

--- book-1-foundation/Invoke-AutoinstallTest.ps1
+++ book-1-foundation/Invoke-AutoinstallTest.ps1
@@ -1,6 +1,6 @@
 param([switch]$SkipCleanup)
 
-. "$PSScriptRoot\SDK.ps1"
+. "$PSScriptRoot\..\book-0-builder\host-sdk\SDK.ps1"
 
 # Assumes ISO was already built (artifacts.iso populated)
 # Run autoinstall tests (ISO path queried from artifacts automatically)
