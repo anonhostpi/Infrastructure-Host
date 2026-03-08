77244338a1355df07d3b0c5cadca46caaa6cf2b9
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:34:23 -0700

    feat(host-sdk): wire Renderer.ps1 into SDK module load sequence

--- book-0-builder/host-sdk/SDK.ps1
+++ book-0-builder/host-sdk/SDK.ps1
@@ -80,6 +80,7 @@ New-Module -Name SDK -ScriptBlock {
     & "$PSScriptRoot/modules/Multipass.ps1" -SDK $SDK
     & "$PSScriptRoot/modules/Builder.ps1" -SDK $SDK
     & "$PSScriptRoot/modules/Fragments.ps1" -SDK $SDK
+    & "$PSScriptRoot/modules/Renderer.ps1" -SDK $SDK
     & "$PSScriptRoot/modules/Testing.ps1" -SDK $SDK
     & "$PSScriptRoot/modules/Verifications.ps1" -SDK $SDK
     & "$PSScriptRoot/modules/CloudInit.ps1" -SDK $SDK
