d976274804a40ce938934d581f1c73febf7e7cbe
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:49:51 -0700

    style(ipy): condense _sha512_crypt rounds loop if/else to single lines

--- book-0-builder/ipy/filters.py
+++ book-0-builder/ipy/filters.py
@@ -90,22 +90,15 @@ def _sha512_crypt(password, salt, rounds):
         i -= 64
     s += ds[:i]
 
-    # Main loop
     c = a
     for i in range(rounds):
         ctx = hashlib.sha512()
-        if i & 1:
-            ctx.update(p)
-        else:
-            ctx.update(c)
-        if i % 3:
-            ctx.update(s)
-        if i % 7:
-            ctx.update(p)
-        if i & 1:
-            ctx.update(c)
-        else:
-            ctx.update(p)
+        if i & 1: ctx.update(p)
+        else: ctx.update(c)
+        if i % 3: ctx.update(s)
+        if i % 7: ctx.update(p)
+        if i & 1: ctx.update(c)
+        else: ctx.update(p)
         c = ctx.digest()
 
     # Encode result in base64-like format
