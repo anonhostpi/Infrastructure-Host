16aff7ce66ff7a602b1f687c6c5a875dbf72d282
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:33:38 -0700

    feat(ipy): add engine.ps1 shape with region markers and stubs

---
+++ book-0-builder/ipy/engine.ps1
@@ -0,0 +1,18 @@
+param(
+    [Parameter(Mandatory = $true)]
+    $SDK
+)
+
+#region MARK: Bootstrap IronPython
+$ipy_engine = $null  # WIP
+#endregion
+
+#region MARK: Load ipy.Yaml
+# WIP
+#endregion
+
+#region MARK: Load ipy.Jinja
+# WIP
+#endregion
+
+return $ipy_engine
