4763953bb5dfdb00ade03e486ed56db564be0128
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:33:47 -0700

    feat(ipy): implement IronPython bootstrap with ipy.Yaml and ipy.Jinja

--- book-0-builder/ipy/engine.ps1
+++ book-0-builder/ipy/engine.ps1
@@ -4,15 +4,18 @@ param(
 )
 
 #region MARK: Bootstrap IronPython
-$ipy_engine = $null  # WIP
+$ipy_loader = Invoke-WebRequest 'https://raw.githubusercontent.com/anonhostpi/IronPythonEmbedded/main/IronPythonEmbedded.ps1' -UseBasicParsing
+$ipy_engine = & ([scriptblock]::Create($ipy_loader.Content))
 #endregion
 
 #region MARK: Load ipy.Yaml
-# WIP
+$ipy_yaml_loader = Invoke-WebRequest 'https://raw.githubusercontent.com/anonhostpi/ipy.Yaml/main/ipy.Yaml.ps1' -UseBasicParsing
+& ([scriptblock]::Create($ipy_yaml_loader.Content)) -Engine $ipy_engine
 #endregion
 
 #region MARK: Load ipy.Jinja
-# WIP
+$ipy_jinja_loader = Invoke-WebRequest 'https://raw.githubusercontent.com/anonhostpi/ipy.Jinja/main/ipy.Jinja.ps1' -UseBasicParsing
+& ([scriptblock]::Create($ipy_jinja_loader.Content)) -Engine $ipy_engine
 #endregion
 
 return $ipy_engine
