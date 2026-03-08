1e04af7236750138244c559e8e2df3cf57126f2d
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:31:31 -0700

    feat(ipy): implement to_base64, shell_quote, shell_array filters

--- book-0-builder/ipy/filters.py
+++ book-0-builder/ipy/filters.py
@@ -7,9 +7,17 @@ import os
 from ruamel.yaml import YAML as _YAML
 
 
-def to_base64(value): pass  # WIP
-def shell_quote(value): pass  # WIP
-def shell_array(items): pass  # WIP
+def to_base64(value):
+    """Encode string to base64."""
+    return base64.b64encode(value.encode('utf-8')).decode('ascii')
+
+def shell_quote(value):
+    """Escape for shell single quotes."""
+    return "'" + str(value).replace("'", "'\\''") + "'"
+
+def shell_array(items):
+    """Convert list to bash array literal."""
+    return '(' + ' '.join(shell_quote(i) for i in items) + ')'
 def ip_only(cidr_notation): pass  # WIP
 def cidr_only(cidr_notation): pass  # WIP
 def to_yaml(value): pass  # WIP
