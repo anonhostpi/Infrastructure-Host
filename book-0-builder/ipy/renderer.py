336abcbfdf9ca0ff66e81b36eefb03f5d5364306
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:52:45 -0700

    fix(ipy): remove layer/for_iso filter block from render_cloud_init

--- book-0-builder/ipy/renderer.py
+++ book-0-builder/ipy/renderer.py
@@ -50,17 +50,6 @@ def render_cloud_init(ctx, env, fragments):
         tpl_path = os.path.join(frag['_path'], 'fragment.yaml.tpl')
         if not os.path.exists(tpl_path):
             continue
-
-        elif layer is not None:
-            # Filter by layer (if specified)
-            frag_layer = fragment.get('build_layer', 999)
-            # build_layer can be int or list of ints
-            if isinstance(frag_layer, list):
-                if not any(l <= layer for l in frag_layer):
-                    continue
-            elif frag_layer > layer:
-                continue
-
         template_path = tpl_path.as_posix()
         rendered = render_text(ctx, template_path, scripts=scripts)
 
